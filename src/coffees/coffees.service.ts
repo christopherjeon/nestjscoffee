d78c0128c65f4e0d3497f3595ac6b58343ef1395
Chris Jeon <cjeon97@gmail.com>
2021-05-03 16:09:42 -0400

    create non-class provider token

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus, NotFoundException, Inject } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Event } from 'src/events/entities/event.entity';
 import { Connection, Repository } from 'typeorm';
+import { COFFEE_BRANDS } from './coffees.constants';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
@@ -15,7 +16,8 @@ export class CoffeesService {
         private readonly coffeeRepository: Repository<Coffee>,
         @InjectRepository(Flavor)
         private readonly flavorRepository: Repository<Flavor>,
-        private readonly connection: Connection
+        private readonly connection: Connection,
+        @Inject(COFFEE_BRANDS) coffeeBrands: string[],
     ) { }
 
     findAll(paginationQuery: PaginationQueryDto) {
